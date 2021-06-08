758f634a6b935f4f9775a57c0bf37c1de25352d2
rafapgomes <pimentelgomes17@gmail.com>
2021-06-08 11:33:44 -0300

    update principal.py

--- principal.py
+++ principal.py
@@ -1,4 +1,16 @@
+from bot import twitter
 import arq
+import scrapepage
+import twittervid
+
+#pega os links da midia do instagram
+def get_insta_post(url):
+    page = scrapepage.get_photo_page(url)
+    json_text = scrapepage.get_json_media_page(page)
+    vetor = scrapepage.get_download_link(json_text)
+    return vetor
+
+#envia multiplas midias
 def envio_sidecar(update,vetor):
         cont=0
         for i in vetor:
@@ -12,12 +24,23 @@ def envio_sidecar(update,vetor):
 
             cont = cont+1
    
-
+#envia unica foto
 def envio_single_photo(update,vetor):
     arq.download(vetor,'midia/1','.jpeg')
     update.message.reply_photo(photo=open('midia/1.jpeg','rb'))
 
+#envia unico video
 def envio_single_video(update,vetor):
     arq.download(vetor,'midia/1','.mp4')
-    print("teste",vetor)
     update.message.reply_video(video=open('midia/1.mp4','rb'))
+
+#envia unico video do twitter
+def envio_twitter(update,url):
+
+    id = twittervid.spliturl(url)
+    print(id)
+    url = twittervid.getvideourl(id)
+    arq.download(url,'ttvid/1','.mp4')
+    update.message.reply_text('Enviando video!')
+    update.message.reply_video(video=open('ttvid/1.mp4','rb'))
+    
