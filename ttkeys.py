5357b535bb22767c1deddf1f2ce892f3fb4642c3
rafapgomes <pimentelgomes17@gmail.com>
2021-06-08 11:33:57 -0300

    ttkeys.py

---
+++ ttkeys.py
@@ -0,0 +1,7 @@
+import os
+
+
+consumer_key = os.environ.get("consumer_key")
+consumer_secret = os.environ.get("consumer_secret")
+access_token = os.environ.get("access_token")
+access_token_secret = os.environ.get("access_token_secret")
