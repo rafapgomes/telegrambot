669c2adfbaa6659191f95606ec647775f82b76a4
rafapgomes <pimentelgomes17@gmail.com>
2021-10-21 19:29:38 -0300

    commit

--- twittervid.py
+++ twittervid.py
@@ -1,9 +1,8 @@
 import ttkeys
 import tweepy
-import json
-autenticacao = tweepy.OAuthHandler('1jPRRn1vwAbWust2acpvz4bJF','uDOuGbws7R4gJgOf5txQnsuFOlOjFjLhTi6Pvz1immfQq19Te8')
+autenticacao = tweepy.OAuthHandler(ttkeys.consumer_key,ttkeys.consumer_secret)
 
-autenticacao.set_access_token('1371708586926219266-m1m9qf5EJlQHfONDOp4LTac9wgKYM1','LNuZhrm7E7FHO31p9PkrAXUIMHCMrFVKthbAhJ6FseJWp')
+autenticacao.set_access_token(ttkeys.access_token,ttkeys.access_token_secret)
 
 twitter = tweepy.API(autenticacao)
 
@@ -15,7 +14,7 @@ def getvideourl(id):
     tweet = twitter.get_status(id,tweet_mode="extended")
     for i in tweet.extended_entities['media'][0]['video_info']['variants']:
        if i['content_type'] == 'video/mp4':
-                return i
+                return i['url']
 
 
 
