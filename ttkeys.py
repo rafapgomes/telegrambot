30f2eea54c81196ead387b9cbc16daaf00ef555e
rafapgomes <pimentelgomes17@gmail.com>
2022-02-28 20:45:19 -0300

    commit

--- ttkeys.py
+++ ttkeys.py
@@ -1,7 +1,5 @@
 import os
-
-
-consumer_key = os.environ.get("consumer_key")
-consumer_secret = os.environ.get("consumer_secret")
-access_token = os.environ.get("access_token")
-access_token_secret = os.environ.get("access_token_secret")
+consumer_key = "AvSnlE2PKb2ZWsyNYeUTVMo0T"
+consumer_secret = "5TQ0x8DKiFNjm481GH2AD00rLS4WB5Y38zEk9xQK8FTLL69IjA"
+access_token = "1459268331027025923-6FdUCRdrqIxIUQlJnv3mWOfUl57Yyk"
+access_token_secret = "1459268331027025923-6FdUCRdrqIxIUQlJnv3mWOfUl57Yyk"
