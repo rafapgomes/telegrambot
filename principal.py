84aeea8428e07a9fa69b30034e43679276a11f4a
rafapgomes <pimentelgomes17@gmail.com>
2023-07-13 16:35:28 -0300

    commit

--- principal.py
+++ principal.py
@@ -1,21 +1,15 @@
-import arq
 import cbf_scraper
-
     
-def envia_info_jogos(update,info_time):
-    rodada = info_time['rodada']
+def envia_info_jogo(rodada,info_time):
+    #desmembra as informações do time
     time = info_time['time']
     divisao = info_time['div']
-    rodada = int(rodada)
-    cont=rodada
-    if cont > 0:
-        cont=rodada-1
-    num = int(rodada)+3
-    for i in range(cont,num):
-        jogo = cbf_scraper.get_jogo(i,time,divisao)
-        info = cbf_scraper.get_info_jogo(jogo)
-        link = info['link']
-        transmissao = cbf_scraper.get_info_partida(link)
-        update.message.reply_text('Rodada '+str(i+1)+'\n'+'Data:'+ info['desc']+'\n'+info['casa'] + " " + info['info_geral'] + " " + info['fora']+'\n'+'Transmissao: '+transmissao)
-        
+    #pega as informações do jogo
+    jogo = cbf_scraper.get_jogo(rodada,time,divisao)
+    info = cbf_scraper.get_info_jogo(jogo)
+    link = info['link']
+    transmissao = cbf_scraper.get_info_partida(link)
+    #retorna as informações de um jogo
+    return info,transmissao
+     
 
