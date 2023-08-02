26e4661443dd593ae6adab78b9d582209e012724
rafapgomes <pimentelgomes17@gmail.com>
2023-08-02 20:07:38 -0300

    commit

--- principal.py
+++ principal.py
@@ -1,12 +1,17 @@
 import cbf_scraper
+import requests.exceptions as re
 
 def envia_info_jogo(rodada,info_time):
     #desmembra as informações do time
     time = info_time['time']
     divisao = info_time['div']
     #pega as informações do jogo
-    jogo = cbf_scraper.get_jogo(rodada,time,divisao)
+    try:
+        jogo = cbf_scraper.get_jogo(rodada,time,divisao)
+    except re.ConnectTimeout:
+        print("Erro")
     info = cbf_scraper.get_info_jogo(jogo)
+    
     link = info['link']
     transmissao = cbf_scraper.get_info_partida(link)
     #retorna as informações de um jogo
