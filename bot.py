9dfc11a14b326e5621211da2031a0fc0cefc5daa
rafapgomes <pimentelgomes17@gmail.com>
2024-01-30 02:05:13 -0300

    dividi em funcoes para melhorar legibilidade

--- bot.py
+++ bot.py
@@ -1,13 +1,11 @@
 import telebot
-import cbf_scraper
 import principal
 import dicionariotimes
-import os
 from dotenv import load_dotenv
 import requests.exceptions as re
-
+import time_info
 load_dotenv()
-chave_api = os.getenv("TOKEN")
+chave_api = "6964674281:AAEQtXNNnqSGi3FpwS_K1Ith_KP_inzxoAU"
 
 bot = telebot.TeleBot(str(chave_api))
 
@@ -28,43 +26,31 @@ def start(mensagem):
 def time(mensagem):
 
     time = " ".join(mensagem.text.split(" ")[1:])
-    try:
-        booleano = time.upper() in dicionariotimes.siglas
-        print(booleano)
-        if  not booleano:
-            raise KeyError("Time não encontrado no dicionário.")
+    #verifica se o time digitado existe    
+    if(time_info.verificar_time_existente == False):
+         bot.send_message("Time nao encontrado,tente novamente")
+         return
 
-        bot.send_message(mensagem.chat.id,"Buscando informações de jogos do " + dicionariotimes.siglas[time.upper()][0] +", aguarde")
-        try:
-            info_time = cbf_scraper.get_rodada(time.upper())
-        except re.ConnectTimeout:
-            print('Nao foi possivel acessar o site da cbf')
-            bot.send_message(mensagem.chat.id,"Nao foi possivel acessar o site da cbf, tente novamente")
-            return -1
-        except re.ReadTimeout:
-             print('Nao foi possivel acessar os dados,tente novamente')
-             bot.send_message(mensagem.chat.id,"Nao foi possivel acessar o site da cbf, tente novamente")
-             return -1
-
-        rodada = info_time['rodada']
-        rodada = int(rodada)
-        cont = rodada
-        for i in range(cont-1,rodada + 3):
-            try:
-                info_jogo,transmissao = principal.envia_info_jogo(i,info_time)
-            except re.ConnectTimeout:
-                print('Nao foi possivel acessar o site da cbf')
-                bot.send_message(mensagem.chat.id,"Nao foi possivel acessar o site da cbf, tente novamente")
-                return -1
-            except re.ReadTimeout:
-             print('Nao foi possivel acessar os dados,tente novamente')
-             bot.send_message(mensagem.chat.id,"Nao foi possivel acessar o site da cbf, tente novamente")
-             return -1
-            bot.send_message(mensagem.chat.id,'Rodada '+ str(i)  +'\n'+'Data:'+ info_jogo['desc']+'\n'+info_jogo['casa'] + " " + 
-            info_jogo['info_geral'] + " " + info_jogo['fora']+'\n'+'Transmissao: '+transmissao)
-    except KeyError as e:
-        bot.send_message(mensagem.chat.id, "Erro: Time não encontrado")
-        print("Erro:", e)
+    bot.send_message(mensagem.chat.id,"Buscando informações de jogos do " + dicionariotimes.siglas[time.upper()][0] +", aguarde")
+    #chama a funcao que pega as informacoes do time desejado
+    info_time = time_info.get_info_time(time)
+    if(info_time == -1):
+        bot.send_message(mensagem.chat.id,"Ocorreu um erro,tente novamente")
+        return
+        
+    rodada = info_time['rodada']
+    rodada = int(rodada)
+    cont = rodada
+    for i in range(cont-1,rodada + 3):
+        info_jogo,transmissao = principal.get_info_jogo(i,info_time)
+        if(info_jogo  == -1 or transmissao == -1):
+            bot.send_message(mensagem.chat.id,"Ocorreu um erro ao acessar as informações do jogo")
+            return
+        if(info_jogo == "rodada38"):
+             bot.send_message(mensagem.chat.id,"Ultima rodada do campeonato foi mostrada acima")
+             return
+        bot.send_message(mensagem.chat.id,'Rodada '+ str(i)  +'\n'+'Data:'+ info_jogo['desc']+'\n'+info_jogo['casa'] + " " + 
+        info_jogo['info_geral'] + " " + info_jogo['fora']+'\n'+'Transmissao: '+transmissao)
 
 
 @bot.message_handler(func=lambda m: True)
