9c918203c52183a9c97e9d723251a0a8ec224495
rafapgomes <pimentelgomes17@gmail.com>
2024-02-03 01:03:00 -0300

    retirado tratamento de excecao

--- getpage.py
+++ getpage.py
@@ -1,14 +1,7 @@
 import requests
 
 def request(url,h,c,t):
-    try:  
         response = requests.get(url,headers=h,cookies=c,timeout=t)
         return response.content
-    except requests.ConnectTimeout:
-        print("Erro, nao foi possivel acessar a pagina")
-        raise
-    except requests.ReadTimeout:
-        print("Nao foi possivel acessar os dados")
-        raise
    
 
